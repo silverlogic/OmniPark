275a396d5e0ab4fc9d7effac5b78d399895ceb33
Manny Guerrero <emanuelguerrerodev@gmail.com>
2018-01-23 15:23:45 -0500

    Revert "Add fabric and crashlytics "

--- OmniPark/AppDelegate.swift
+++ OmniPark/AppDelegate.swift
@@ -9,8 +9,6 @@
 import UIKit
 import UserNotifications
 import GoogleMaps
-import Fabric
-import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        Fabric.with([Crashlytics.self])
         GMSServices.provideAPIKey(GOOGLE_MAPS_API_KEY)
         UserManager.shared.login()
         let current = UNUserNotificationCenter.current()
